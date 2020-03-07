f0fba11b7eaded75e61ef7e774d49f614ce42f4e
Omoleye Oluseye <sheye009@yahoo.com>
2020-03-07 06:44:45 +0100

    Another testing merge update

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/MyLayout.js'
 export default function About() {
   return (
     <Layout>
-      <p>This is the about pagexxxxx</p>
+      <p>This is the about pagexxxxxx</p>
     </Layout>
   )
 }
