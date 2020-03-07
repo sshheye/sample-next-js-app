bc729d4ce843f5bb89b56625c6f4885b0d28edc0
Omoleye Oluseye <sheye009@yahoo.com>
2020-03-07 07:50:26 +0100

    none conflict

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Layout from '../components/MyLayout.js'
 export default function About() {
   return (
     <Layout>
-      <p>This is the about pagexxxyx</p>
+      <p>This is the about pagexxyxyx</p>
     </Layout>
   )
 }
